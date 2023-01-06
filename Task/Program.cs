1b278a9fa17d000b62f4c377bb25aca4275e7195
chicheva <chicheva-e@mail.ru>
2023-01-06 23:48:45 +0300

    Добавлен метод сортировки массива

--- Task/Program.cs
+++ Task/Program.cs
@@ -10,6 +10,20 @@
 Console.WriteLine("Введите массив через пробел: ");
 string[] array = Console.ReadLine().Split().ToArray();
 
+string[] SortArray (string[] arr1){
+    string[] tempArr = new string[arr1.Length];
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            tempArr[count] = arr1[i];
+            count++;
+        }
+    }
+    return tempArr;
+}
+
 void PrintArray (string[] arr){
     for (int i = 0; i < array.Length; i++)
     {
